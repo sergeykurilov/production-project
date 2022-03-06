0775f45c40730efc23a058caa087214d3f08e963
TIm <akva_ten@mail.ru>
2022-03-06 13:49:16 +0300

    add storybook, loki

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,7 +45,7 @@ module.exports = {
             'error',
             {
                 markupOnly: true,
-                ignoreAttribute: ['data-testid'],
+                ignoreAttribute: ['data-testid', 'to'],
             },
         ],
         'max-len': ['error', { ignoreComments: true, code: 100 }],
