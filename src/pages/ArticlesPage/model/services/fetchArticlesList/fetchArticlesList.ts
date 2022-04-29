ddcac81be93b9d0563d91053bb5d1e54b3a5351f
TIm <akva_ten@mail.ru>
2022-04-29 17:24:57 +0300

    add infinite scroll

--- src/pages/ArticlesPage/model/services/fetchArticlesList/fetchArticlesList.ts
+++ src/pages/ArticlesPage/model/services/fetchArticlesList/fetchArticlesList.ts
@@ -2,20 +2,29 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { ThunkConfig } from 'app/providers/StoreProvider';
 import { Comment } from 'entities/Comment';
 import { Article } from 'entities/Article';
+import { getArticlesPageLimit } from 'pages/ArticlesPage/model/selectors/articlesPageSelectors';
+
+interface FetchArticlesListProps {
+    page?: number;
+}
 
 export const fetchArticlesList = createAsyncThunk<
     Article[],
-    void,
+    FetchArticlesListProps,
     ThunkConfig<string>
     >(
         'articlesPage/fetchArticlesList',
-        async (articleId, thunkApi) => {
-            const { extra, rejectWithValue } = thunkApi;
+        async (props, thunkApi) => {
+            const { extra, rejectWithValue, getState } = thunkApi;
+            const { page = 1 } = props;
+            const limit = getArticlesPageLimit(getState());
 
             try {
                 const response = await extra.api.get<Article[]>('/articles', {
                     params: {
                         _expand: 'user',
+                        _limit: limit,
+                        _page: page,
                     },
                 });
 
---
+++ src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
@@ -0,0 +1,36 @@
+import { MutableRefObject, useEffect, useRef } from 'react';
+
+export interface UseInfiniteScrollOptions {
+    callback?: () => void;
+    triggerRef: MutableRefObject<HTMLElement>;
+    wrapperRef: MutableRefObject<HTMLElement>;
+}
+
+export function useInfiniteScroll({ callback, wrapperRef, triggerRef }: UseInfiniteScrollOptions) {
+    const observer = useRef<IntersectionObserver | null>(null);
+
+    useEffect(() => {
+        if (callback) {
+            const options = {
+                root: wrapperRef.current,
+                rootMargin: '0px',
+                threshold: 1.0,
+            };
+
+            observer.current = new IntersectionObserver(([entry]) => {
+                if (entry.isIntersecting) {
+                    callback();
+                }
+            }, options);
+
+            observer.current.observe(triggerRef.current);
+        }
+
+        return () => {
+            if (observer.current) {
+                // eslint-disable-next-line react-hooks/exhaustive-deps
+                observer.current.unobserve(triggerRef.current);
+            }
+        };
+    }, [callback, triggerRef, wrapperRef]);
+}
