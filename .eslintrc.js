7e2ba5cbf80d4254d21e5099ad04726dc0645c37
TIm <akva_ten@mail.ru>
2022-07-16 17:17:08 +0300

    add HStack, VStack, Flex

--- .eslintrc.js
+++ .eslintrc.js
@@ -47,7 +47,7 @@ module.exports = {
             'error',
             {
                 markupOnly: true,
-                ignoreAttribute: ['data-testid', 'to', 'target'],
+                ignoreAttribute: ['data-testid', 'to', 'target', 'justify', 'align', 'direction', 'gap'],
             },
         ],
         'max-len': ['error', { ignoreComments: true, code: 125 }],
