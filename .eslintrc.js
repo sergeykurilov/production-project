a962063ed924cf497ca8b03498f56ade0599513f
TIm <akva_ten@mail.ru>
2022-09-19 20:21:08 +0300

    add new eslint plugin, fix errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -71,6 +71,7 @@ module.exports = {
         'react/no-array-index-key': 'off',
         'arrow-body-style': 'off',
         'ulbi-tv-plugin/path-checker': ['error', { alias: '@' }],
+        'ulbi-tv-plugin/public-api-imports': ['error', { alias: '@' }],
     },
     globals: {
         __IS_DEV__: true,
