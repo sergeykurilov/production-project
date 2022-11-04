1912ac67d976daf622be046347326ea0a9d1bbf8
TIm <akva_ten@mail.ru>
2022-11-04 11:36:05 +0300

    add appImage, fallback, errorFallback

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Page } from '@/widgets/Page';
-import { Counter } from '@/entities/Counter';
 
 const MainPage = () => {
     const { t } = useTranslation();
@@ -13,7 +12,6 @@ const MainPage = () => {
 
     return (
         <Page>
-            <Counter />
             {t('Главная страница')}
         </Page>
     );
